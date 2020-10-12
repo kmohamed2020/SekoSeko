74b4b2bf2f9595d96709887de479c4dfb2a26c35
Khaled Salem <khaledfawzy@outlook.com>
2020-10-12 15:14:21 +0200

    Update InvokeFun.ps1

--- InvokeFun.ps1
+++ InvokeFun.ps1
@@ -1,10 +1,9 @@
-﻿function InvokeFun{
+﻿function InvokeFun {
 
 	[CmdletBinding(DefaultParameterSetName="reverse")] Param(
 
         [Parameter(Position = 0, Mandatory = $false, ParameterSetName="reverse")]
         
-
     )
 
 write-host "Test";
